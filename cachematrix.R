c7dc50486aefcd6ad6fa52b0932bb2d4805012af
Dmitry Ornatsky <dornatsky@gmail.com>
2017-06-08 02:07:08 -0500

    implemented inverse matrix calculations As prescribed, the solution uses memoization in an eclosing function local variable.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Creates a matrix wrapper, which, in addition to storing the data itself, 
+## contains functions allowing the caller to calculate an inverse matrix 
+## and memoize the result.
+## The interface will be later used by cacheSolve to retrieve the calculated 
+## value.
+## Use of camel case is deliberate, convention used in the sample is hard to read.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  memoizedInverse <- NULL
+  
+  # I got rid of the set function since the wrapping closure does the job just fine
+  get <- function() x
+  setInverse <- function(inverse) memoizedInverse <<- inverse
+  getInverse <- function() memoizedInverse
+  list(get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# Actually computes the the inverse, using the memoized solution, if present
+# Tested with diagonal matrices and
+# m <- matrix (c(4, 2, 7, 6), 2, 2)
+# x<-makeCacheMatrix(diag(3))
+# cacheSolve(x)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  memoizedInverse <- x$getInverse()
+  if(!is.null(memoizedInverse)) {
+    message("getting cached data")
+    return(memoizedInverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$setInverse(inverse)
+  inverse
 }
